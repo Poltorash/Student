843e7b7a1df385a02d4da0d2464dab7ec54050cc
Poltorash <mad.lemons@mail.ru>
2021-02-04 12:28:48 +0500

    Добавьте файлы проекта.

---
+++ Student/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Student
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (var db = new StudentModel()) 
+            {
+                ComboLog.ItemsSource = db.Curators.ToList();
+            }
+        }
+
+        private void BtnAutorization_Click(object sender, RoutedEventArgs e)
+        {
+            bool key = false;
+            using (var db = new StudentModel()) 
+            {
+                foreach (var autorization in db.Curators.ToList()) 
+                {
+                    if (ComboLog.SelectedItem.ToString() == autorization.LastName && PasswordB.Password == autorization.Password) 
+                    {
+                       (new MenuWindow()).Show();                      
+                        key = true;
+                        this.Close();
+                    }
+                }
+            }
+            if (key == false) 
+            {
+                MessageBox.Show("Неверный пароль");
+            }
+        }
+    }
+}
---
+++ Student/StudentModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Student
+{
+    public class StudentModel : DbContext
+    {
+        // Контекст настроен для использования строки подключения "StudentModel" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "Student.StudentModel" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "StudentModel" 
+        // в файле конфигурации приложения.
+        public StudentModel()
+            : base("name=StudentModel")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        public virtual DbSet<Group> Groups { get; set; }
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Curator> Curators { get; set; }
+    }
+
+    public class Group
+    {
+        public int GroupID { get; set; }
+        public string Title { get; set; }
+
+        public Curator Curator { get; set; }
+        public virtual ICollection<Student> Students { get; set; }
+    }
+    public class Curator
+    {
+        public int CuratorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Password { get; set; }
+        public int? GroupID { get; set; }
+        public Group Group { get; set; }
+    }
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Year_of_admission { get; set; }
+        public int? GroupID { get; set; }
+        public Group Group { get; set; }
+        
+    }
+}
